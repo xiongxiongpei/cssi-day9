a3ee94b0d66741794bc59c0af295d82621685a19
Ade Adedayo <aadedayo@cssi-online.co>
2021-07-22 05:10:05 +0000

    Remove day 8 code

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -5,22 +5,3 @@ const getMessages = () => {
     findMessage(data);
     });
 }
-
-const findMessage = (messages) => {
-    const passcodeAttempt = document.querySelector('#passcode').value;
-    for(message in messages) {
-        const messageData = messages[message];
-        if(messageData.passcode === passcodeAttempt) {
-            renderMessageAsHtml(messageData.message)
-        }
-    }
-}
-
-const renderMessageAsHtml = (message) => {
-    // Hide Input Form
-    const passcodeInput = document.querySelector('#passcodeInput');
-    passcodeInput.style.display = 'none';
-    // Render messageas HTML
-    const messageDiv = document.querySelector('#message');
-    messageDiv.innerHTML = message;   
-}
