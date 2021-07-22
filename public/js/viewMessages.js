aefc0b1c2c9cba4e2480fb6ec0c56bc7d12e4cc3
Ade Adedayo <aadedayo@cssi-online.co>
2021-07-22 16:55:08 +0000

    Updated viewMessages logic

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -1,7 +1,26 @@
+// Retrieve the messages from the database
 const getMessages = () => {
-    const messagesRef = firebase.database().ref();
-    messagesRef.on('value', (snapshot) => {
-    const data = snapshot.val();
-    findMessage(data);
-    });
+ const messagesRef = firebase.database().ref('/messages');
+ messagesRef.on('value', (snapshot) => {
+     const data = snapshot.val();
+     console.log(data);
+     // Find message
+ });
+}
+
+const findMessage = (messages) => {
+ const passcodeAttempt = document.querySelector('#passcode').value;
+ for (message in messages) {
+     const messageData = messages[message];
+     if (messageData.passcode === passcodeAttempt) {
+         // Code to hide input form, and render message as HTML
+     }
+ }
+}
+
+
+const renderMessageAsHtml = (message) => {
+ // Hide Input Form
+
+ // Render messageas HTML
 }
